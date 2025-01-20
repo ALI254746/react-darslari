e53a2870445b126f76fdbd7380d7479b676401da
ALI254746! <hojiakbarmuhammad77@gmail.com>
2025-01-20 22:30:00 +0500

    kino loyiha search filter info qisimlari

---
+++ src/components/app/app.js
@@ -0,0 +1,21 @@
+import AppFilter from "../app-filter/app-filter";
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import './app.css'
+const App =()=>{
+    return (
+    <div className="app font-monospace">
+        <div className="content">
+            <AppInfo/>
+        </div>
+        <div className="search-panel">
+            <SearchPanel/>
+        </div>
+        <div >
+            <AppFilter/>
+        </div>
+        
+        
+     </div>)
+}
+export default App;
