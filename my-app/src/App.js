42ae7a82d82aca3c765b3b4cbf366d00a54afcd6
ALI254746! <hojiakbarmuhammad77@gmail.com>
2025-01-20 16:36:11 +0500

    react 2-dars

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Headr=()=>{
+  const text='salom ali'
+  const getText=(text)=>{
+    return(
+      <div>
+        <h1>hello {text}</h1>
+        <p>oldin git hubga kirib repazitoriy ochib olish kerakmi
+        Ha, to'g'ri. Oldin GitHub'da yangi repozitoriy yaratishingiz kerak. Buning uchun quyidagi amallarni bajaring:</p>
+      </div>
+    )
+
+  }
+
+
+  return <div>{getText('ali')}</div>
+}
+const Field=()=>{
+  return <input type='text' placeholder='writing..'/>
+
+}
+const Button=()=>{
+  const user=true
+  const login='login'
+  return <button>{user ? 'logout':'login'}</button>
+}
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Headr/>
+      <Field/>
+      <Button/>
     </div>
-  );
+  )
+   
 }
 
 export default App;
