d2fe9b911e803cea20726998dd7130f4810f23ed
ALI254746! <hojiakbarmuhammad77@gmail.com>
2025-02-03 19:00:46 +0500

    formalar bilan ishlash

--- src/components/kino-add-form/kino-add-form.js
+++ src/components/kino-add-form/kino-add-form.js
@@ -1,18 +1,38 @@
+import { Component } from 'react'
 import './kino-add-form.css'
 
-const KinoAddForm = () => {
-  return (
-    <div class='kino-add-form'>
-        <h3>Yangi kinolar qo'shish</h3>
-      <form className='add-from d-flex'>
-        <input type='text' className='form-control new-post-label' placeholder='Qanday Kino'/>
-        <input type='number' className='form-control new-post-label' placeholder='Nechi marotaba korilgan'/>
-        <button type="submit" className='btn btn-outline-dark'>
-            Qo'shish
-        </button>
-      </form>
-    </div>
-  )
+class KinoAddForm extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      name:'+998',
+      viewars:'12'
+    
+  }
+
+   
+  }
+  changeHandlerInput=(e)=>{
+    this.setState({
+      [e.target.name]:e.target.value,
+})
+  }
+  render(){
+    const {name,viewars}=this.state
+    return (
+      <div className='kino-add-form'>
+          <h3>Yangi kinolar qo'shish</h3>
+        <form className='add-from d-flex'>
+          <input value={name} name="name" onChange={this.changeHandlerInput} type='text' className='form-control new-post-label' placeholder='Qanday Kino'/>
+          <input value={viewars} name="viewars" onChange={this.changeHandlerInput}  type='number' className='form-control new-post-label' placeholder='Nechi marotaba korilgan'/>
+          <button type="submit" className='btn btn-outline-dark'>
+              Qo'shish
+          </button>
+        </form>
+      </div>
+    )
+  }
+ 
 }
 
 export default KinoAddForm
